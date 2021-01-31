2bb0e54c0e8fb6caf92ad7da3b20c9953d0d7757
Hessam Mehr <hessammehr@gmail.com>
2021-01-31 13:30:28 +0000

    Derive capabilities based on device type.

--- capabilities.pl
+++ capabilities.pl
@@ -1,108 +1,30 @@
-can(route('filter1', 'top', 'bottom')).
-can(route('separator1', 'top', 'bottom')).
-can(route('valve4', -1, 0)).
-can(route('valve4', -1, 1)).
-can(route('valve4', -1, 2)).
-can(route('valve4', -1, 3)).
-can(route('valve4', -1, 4)).
-can(route('valve4', -1, 5)).
-can(route('valve4', 0, -1)).
-can(route('valve4', 1, -1)).
-can(route('valve4', 2, -1)).
-can(route('valve4', 3, -1)).
-can(route('valve4', 4, -1)).
-can(route('valve4', 5, -1)).
-can(route('valve5', -1, 0)).
-can(route('valve5', -1, 1)).
-can(route('valve5', -1, 2)).
-can(route('valve5', -1, 3)).
-can(route('valve5', -1, 4)).
-can(route('valve5', -1, 5)).
-can(route('valve5', 0, -1)).
-can(route('valve5', 1, -1)).
-can(route('valve5', 2, -1)).
-can(route('valve5', 3, -1)).
-can(route('valve5', 4, -1)).
-can(route('valve5', 5, -1)).
-can(route('valve1', -1, 0)).
-can(route('valve1', -1, 1)).
-can(route('valve1', -1, 2)).
-can(route('valve1', -1, 3)).
-can(route('valve1', -1, 4)).
-can(route('valve1', -1, 5)).
-can(route('valve1', 0, -1)).
-can(route('valve1', 1, -1)).
-can(route('valve1', 2, -1)).
-can(route('valve1', 3, -1)).
-can(route('valve1', 4, -1)).
-can(route('valve1', 5, -1)).
-can(route('valve2', -1, 0)).
-can(route('valve2', -1, 1)).
-can(route('valve2', -1, 2)).
-can(route('valve2', -1, 3)).
-can(route('valve2', -1, 4)).
-can(route('valve2', -1, 5)).
-can(route('valve2', 0, -1)).
-can(route('valve2', 1, -1)).
-can(route('valve2', 2, -1)).
-can(route('valve2', 3, -1)).
-can(route('valve2', 4, -1)).
-can(route('valve2', 5, -1)).
-can(route('valve0', -1, 0)).
-can(route('valve0', -1, 1)).
-can(route('valve0', -1, 2)).
-can(route('valve0', -1, 3)).
-can(route('valve0', -1, 4)).
-can(route('valve0', -1, 5)).
-can(route('valve0', 0, -1)).
-can(route('valve0', 1, -1)).
-can(route('valve0', 2, -1)).
-can(route('valve0', 3, -1)).
-can(route('valve0', 4, -1)).
-can(route('valve0', 5, -1)).
-can(route('valve3', -1, 0)).
-can(route('valve3', -1, 1)).
-can(route('valve3', -1, 2)).
-can(route('valve3', -1, 3)).
-can(route('valve3', -1, 4)).
-can(route('valve3', -1, 5)).
-can(route('valve3', 0, -1)).
-can(route('valve3', 1, -1)).
-can(route('valve3', 2, -1)).
-can(route('valve3', 3, -1)).
-can(route('valve3', 4, -1)).
-can(route('valve3', 5, -1)).
-can(sink('filter1', 'bottom')).
-can(sink('filter1', 'top')).
-can(sink('separator1', 'bottom')).
-can(sink('separator1', 'top')).
-can(sink('flask0', 0)).
-can(pull('pump4', 0)).
-can(pull('pump5', 0)).
-can(pull('pump1', 0)).
-can(pull('pump2', 0)).
-can(pull('pump3', 0)).
-can(sink('reactor1', 0)).
-can(sink('waste4', 0)).
-can(sink('waste5', 0)).
-can(sink('waste2', 0)).
-can(sink('waste1', 0)).
-can(sink('waste3', 0)).
-can(source('filter1', 'bottom')).
-can(source('flask1', 0)).
-can(source('flask8', 0)).
-can(source('flask3', 0)).
-can(source('flask4', 0)).
-can(source('flask2', 0)).
-can(source('flask10', 0)).
-can(source('flask7', 0)).
-can(source('flask9', 0)).
-can(source('separator1', 'bottom')).
-can(source('flask5', 0)).
-can(source('flask6', 0)).
-can(push('pump4', 0)).
-can(push('pump5', 0)).
-can(push('pump1', 0)).
-can(push('pump2', 0)).
-can(push('pump3', 0)).
-can(source('reactor1', 0)).
+can(source(Flask, 0)) :- flask(Flask).
+
+can(source(Filter, 'bottom')) :- filter(Filter).
+can(sink(Filter, 'bottom')) :- filter(Filter).
+can(sink(Filter, 'top')) :- filter(Filter).
+
+can(pull(Pump, 0)) :- pump(Pump).
+can(push(Pump, 0)) :- pump(Pump).
+
+can(sink(Reactor, 0)) :- reactor(Reactor).
+can(source(Reactor, 0)) :- reactor(Reactor).
+
+can(source(Separator, 'bottom')) :- separator(Separator).
+can(sink(Separator, 'bottom')) :- separator(Separator).
+can(sink(Separator, 'top')) :- separator(Separator).
+
+can(route(Valve, -1, 0)) :- valve(Valve).
+can(route(Valve, -1, 1)) :- valve(Valve).
+can(route(Valve, -1, 2)) :- valve(Valve).
+can(route(Valve, -1, 3)) :- valve(Valve).
+can(route(Valve, -1, 4)) :- valve(Valve).
+can(route(Valve, -1, 5)) :- valve(Valve).
+can(route(Valve, 0, -1)) :- valve(Valve).
+can(route(Valve, 1, -1)) :- valve(Valve).
+can(route(Valve, 2, -1)) :- valve(Valve).
+can(route(Valve, 3, -1)) :- valve(Valve).
+can(route(Valve, 4, -1)) :- valve(Valve).
+can(route(Valve, 5, -1)) :- valve(Valve).
+
+can(sink(Waste, 0)) :- waste(Waste).
